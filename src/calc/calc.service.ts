d83172675eb574e7f8fc020f0a440761b91e70f4
Anni0007 <anirudhbaghel33@gmail.com>
2024-03-30 11:32:43 +0530

    feat: Precendence of operators handled

--- src/calc/calc.service.ts
+++ src/calc/calc.service.ts
@@ -4,10 +4,10 @@ import { CalcDto } from './calc.dto';
 @Injectable()
 export class CalcService {
   calculateExpression(calcBody: CalcDto, res: any) {
-    let expressionArray = calcBody.expression.split('') 
-   
-    // Here we are checking if expression is correct or not
-    // but currently we are not handling the case of brackets in expression
+    let expressionArray = calcBody.expression.split(/([+\-*/])/).filter(a => a.trim() !== '')
+      
+  // Here we are checking if expression is correct or not
+  // but currently we are not handling the case of brackets in expression
     if(expressionArray.length%2 == 0){
       return res.status(400).send({
         "statusCode": 400,
@@ -16,26 +16,44 @@ export class CalcService {
       })
     }
 
-    let result = parseFloat(expressionArray[0]);
+    let normalizedArray = this.getNormalExpression(expressionArray)   
 
-    // iterating a array by skiping a math operator currently not handling a precendence case
-    for (let i = 1; i < expressionArray.length; i += 2) {
-        const mathOperator = expressionArray[i];
-        const nextNumber = parseFloat(expressionArray[i + 1]);
+    let result = parseFloat(normalizedArray[0]);
+
+    for (let i = 1; i < normalizedArray.length; i += 2) {
+        const mathOperator = normalizedArray[i];
+        const nextNumber = parseFloat(normalizedArray[i + 1]);
 
         if (mathOperator === '+') {
             result += nextNumber;
         } else if (mathOperator === '-') {
             result -= nextNumber;
-        } else if (mathOperator === '*') {
-            result *= nextNumber;
-        } else if (mathOperator === '/') {
-            result /= nextNumber;
         }
     }
-
-    console.log("result", result);
     
     return res.status(201).send({result:result})
+  }
+
+  getNormalExpression(expression){
+    let a = expression 
+
+    let b = expression
+
+    for(let i = 0; i <a.length; i++){
+      if(a[i] === '/'){
+        let res = a[i-1]  / a[i+1] 
+        b.splice(i - 1, 3, res.toString()); 
+        i -= 2;
+      }
+    }
+
+    for(let i = 0; i< a.length; i++){
+      if(a[i] === '*'){
+        let res = a[i-1] * a[i+1] 
+        b.splice(i - 1, 3, res.toString()); 
+        i -= 2;
+      }
+    }
+    return b
   }
 }
